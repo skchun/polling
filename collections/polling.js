1e0239168ec821cc928c5a5e5af804b967ccd854
skchun <skchun@hawaii.edu>
2015-10-29 08:34:14 -1000

    Finished WOD

---
+++ collections/polling.js
@@ -0,0 +1,6 @@
+/**
+ * Simple Meteor Polling App. From: https://scotch.io/tutorials/learn-meteor-js-from-scratch-build-a-polling-app
+ * Created by philipjohnson on 8/30/15.
+ */
+
+Polls = new Mongo.Collection('polls');
